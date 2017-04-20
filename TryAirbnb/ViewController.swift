3084c3adefd3cf84d95e7c694eaed7df2c3d7b20
清水一貴 <s.kazutaka55555@gmail.com>
2017-04-20 09:51:12 +0900

    initial comit

---
+++ TryAirbnb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TryAirbnb
+//
+//  Created by 清水一貴 on 2017/04/20.
+//  Copyright © 2017年 清水一貴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
